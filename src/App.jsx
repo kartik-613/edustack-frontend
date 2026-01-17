fb42727485660fa597096f65fb0a8cd010deaa21
Kartik Upadhyay <kartikupadhyay613@gmail.com>
2026-01-17 11:49:39 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='w-full h-screen flex items-center justify-center
+    bg-gray-300'>
+      <h1 className='text-3xl font-bold underline text-center'>
+        Hello, EduStack!
+      </h1>
+    </div>
+  )
+}
+
+export default App
